43cf712fd8e0d483a914e49fa2573edeb1073ba6
venugopalachary <venugopalachary7396@gmail.com>
2019-07-25 22:39:30 +0530

    SmilyRating Example

---
+++ app/src/main/java/com/play4deal/smilyratingexample/MainActivity.java
@@ -0,0 +1,78 @@
+package com.play4deal.smilyratingexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.play4deal.hello.BaseRating;
+import com.play4deal.hello.SmileRating;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG="venu";
+    private   SmileRating smileRating;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         smileRating = (SmileRating) findViewById(R.id.smile_rating);
+        smileRating.setOnSmileySelectionListener(new SmileRating.OnSmileySelectionListener() {
+            @Override
+            public void onSmileySelected(@BaseRating.Smiley int smiley, boolean reselected) {
+                // reselected is false when user selects different smiley that previously selected one
+                // true when the same smiley is selected.
+                // Except if it first time, then the value will be false.
+                switch (smiley) {
+                    case SmileRating.BAD:
+                        Log.i(TAG, "Bad");
+                        break;
+                    case SmileRating.GOOD:
+                        Log.i(TAG, "Good");
+                        break;
+                    case SmileRating.GREAT:
+                        Log.i(TAG, "Great");
+                        break;
+                    case SmileRating.OKAY:
+                        Log.i(TAG, "Okay");
+                        break;
+                    case SmileRating.TERRIBLE:
+                        Log.i(TAG, "Terrible");
+                        break;
+                }
+            }
+        });
+
+        smileRating.setOnRatingSelectedListener(new SmileRating.OnRatingSelectedListener() {
+            @Override
+            public void onRatingSelected(int level, boolean reselected) {
+                // level is from 1 to 5 (0 when none selected)
+                // reselected is false when user selects different smiley that previously selected one
+                // true when the same smiley is selected.
+                // Except if it first time, then the value will be false.
+            }
+        });
+
+        @BaseRating.Smiley int smiley = smileRating.getSelectedSmile();
+        switch (smiley) {
+            case SmileRating.BAD:
+                Log.i(TAG, "Bad");
+                break;
+            case SmileRating.GOOD:
+                Log.i(TAG, "Good");
+                break;
+            case SmileRating.GREAT:
+                Log.i(TAG, "Great");
+                break;
+            case SmileRating.OKAY:
+                Log.i(TAG, "Okay");
+                break;
+            case SmileRating.TERRIBLE:
+                Log.i(TAG, "Terrible");
+                break;
+            case SmileRating.NONE:
+                Log.i(TAG, "None");
+                break;
+        }
+    }
+}
